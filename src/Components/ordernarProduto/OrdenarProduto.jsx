27827b1a24efb696933896275e3be754c007a642
Alexsandro Neves <alexsandrodnss@gmail.com>
2022-07-01 18:10:38 -0300

    ajuste na pagina OrdernaProduto

--- src/Components/ordernarProduto/OrdenarProduto.jsx
+++ src/Components/ordernarProduto/OrdenarProduto.jsx
@@ -3,17 +3,15 @@ import './OrdenarProduto.css'
 
 
 const OrdenarProduto = () => {
-
-    const [ordenar, setOrdenacao] = useState(false);
-        return (
-            <>
-                <span>Ordenar por</span>
-                <select className="container--select">
-                    <option value="selecione-opcao"> Selecione opção</option>
-                    <option value="maior-preco" > Maior Preço</option>
-                    <option value="menor-preco">Menor Preco</option>
-                </select>
-            </>
-        )
-    }
-    export default OrdenarProduto;
+    return (
+        <>
+            <span>Ordenar por</span>
+            <select className="container--select">
+                <option value="selecione-opcao"> Selecione opção</option>
+                <option value="maior-preco" > Maior Preço</option>
+                <option value="menor-preco">Menor Preco</option>
+            </select>
+        </>
+    )
+}
+export default OrdenarProduto;
